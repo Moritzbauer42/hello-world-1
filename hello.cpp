783265df81dfd1dae6ea74a5de14dd6a0776fc5c
Moritzbauer42 <Moritz.Bauer@stud.hs-emden-leer.de>
2023-03-15 16:52:58 +0100

    v3

--- hello.cpp
+++ hello.cpp
@@ -7,5 +7,7 @@ int main()
     cout << "Hallo du kleine Welt bummsi !\n";
     cout <<" das is mal noooooch ein langweiliger test ";
     cout << "Moin!";
+    cout << "holaa !\n";
+    
     return 0;
 }
